22be21f11dffaa9af7fe532095eeec101fac3370
Hendrik Seliger <github@hseliger.eu>
2023-09-10 18:33:35 +0200

    First version of added functions

--- your-scratch-extension/index.js
+++ your-scratch-extension/index.js
@@ -14,14 +14,14 @@ class Scratch3YourExtension {
     getInfo () {
         return {
             // unique ID for your extension
-            id: 'yourScratchExtension',
+            id: 'hseliger-230910',
 
             // name that will be displayed in the Scratch UI
-            name: 'Demo',
+            name: 'CFG IF',
 
-            // colours to use for your extension blocks
-            color1: '#000099',
-            color2: '#660066',
+            // colours to use for your extension blocks (KGSLight and KGSDark)
+            color1: '#019ACD',
+            color2: '#006699',
 
             // icons to display
             blockIconURI: 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAkAAAAFCAAAAACyOJm3AAAAFklEQVQYV2P4DwMMEMgAI/+DEUIMBgAEWB7i7uidhAAAAABJRU5ErkJggg==',
@@ -31,7 +31,7 @@ class Scratch3YourExtension {
             blocks: [
                 {
                     // name of the function where your block code lives
-                    opcode: 'myFirstBlock',
+                    opcode: 'Dec2Hex',
 
                     // type of block - choose from:
                     //   BlockType.REPORTER - returns a value, like "direction"
@@ -41,7 +41,7 @@ class Scratch3YourExtension {
                     blockType: BlockType.REPORTER,
 
                     // label to display on the block
-                    text: 'My first block [MY_NUMBER] and [MY_STRING]',
+                    text: '[MY_NUMBER] nach Hexadezimal',
 
                     // true if this block should end a stack
                     terminal: false,
@@ -56,7 +56,7 @@ class Scratch3YourExtension {
                     arguments: {
                         MY_NUMBER: {
                             // default value before the user sets something
-                            defaultValue: 123,
+                            defaultValue: 42,
 
                             // type/shape of the parameter - choose from:
                             //     ArgumentType.ANGLE - numeric value with an angle picker
@@ -66,10 +66,38 @@ class Scratch3YourExtension {
                             //     ArgumentType.STRING - text value
                             //     ArgumentType.NOTE - midi music value with a piano picker
                             type: ArgumentType.NUMBER
-                        },
+                        }
+                    }
+                },
+                
+                {
+                    // name of the function where your block code lives
+                    opcode: 'Hex2Dec',
+
+                    // type of block - choose from:
+                    //   BlockType.REPORTER - returns a value, like "direction"
+                    //   BlockType.BOOLEAN - same as REPORTER but returns a true/false value
+                    //   BlockType.COMMAND - a normal command block, like "move {} steps"
+                    //   BlockType.HAT - starts a stack if its value changes from false to true ("edge triggered")
+                    blockType: BlockType.REPORTER,
+
+                    // label to display on the block
+                    text: '[MY_STRING] nach Dezimal',
+
+                    // true if this block should end a stack
+                    terminal: false,
+
+                    // where this block should be available for code - choose from:
+                    //   TargetType.SPRITE - for code in sprites
+                    //   TargetType.STAGE  - for code on the stage / backdrop
+                    // remove one of these if this block doesn't apply to both
+                    filter: [ TargetType.SPRITE, TargetType.STAGE ],
+
+                    // arguments used in the block
+                    arguments: {
                         MY_STRING: {
                             // default value before the user sets something
-                            defaultValue: 'hello',
+                            defaultValue: '2A',
 
                             // type/shape of the parameter - choose from:
                             //     ArgumentType.ANGLE - numeric value with an angle picker
@@ -79,6 +107,47 @@ class Scratch3YourExtension {
                             //     ArgumentType.STRING - text value
                             //     ArgumentType.NOTE - midi music value with a piano picker
                             type: ArgumentType.STRING
+                        }
+                    }
+                },
+                
+                {
+                    // name of the function where your block code lives
+                    opcode: 'Code2Char',
+
+                    // type of block - choose from:
+                    //   BlockType.REPORTER - returns a value, like "direction"
+                    //   BlockType.BOOLEAN - same as REPORTER but returns a true/false value
+                    //   BlockType.COMMAND - a normal command block, like "move {} steps"
+                    //   BlockType.HAT - starts a stack if its value changes from false to true ("edge triggered")
+                    blockType: BlockType.REPORTER,
+
+                    // label to display on the block
+                    text: 'Dezimal [MY_NUMBER] in UTF-8 Zeichen',
+
+                    // true if this block should end a stack
+                    terminal: false,
+
+                    // where this block should be available for code - choose from:
+                    //   TargetType.SPRITE - for code in sprites
+                    //   TargetType.STAGE  - for code on the stage / backdrop
+                    // remove one of these if this block doesn't apply to both
+                    filter: [ TargetType.SPRITE, TargetType.STAGE ],
+
+                    // arguments used in the block
+                    arguments: {
+                        MY_NUMBER: {
+                            // default value before the user sets something
+                            defaultValue: 64,
+
+                            // type/shape of the parameter - choose from:
+                            //     ArgumentType.ANGLE - numeric value with an angle picker
+                            //     ArgumentType.BOOLEAN - true/false value
+                            //     ArgumentType.COLOR - numeric value with a colour picker
+                            //     ArgumentType.NUMBER - numeric value
+                            //     ArgumentType.STRING - text value
+                            //     ArgumentType.NOTE - midi music value with a piano picker
+                            type: ArgumentType.NUMBER
                         }
                     }
                 }
@@ -91,9 +160,29 @@ class Scratch3YourExtension {
      * implementation of the block with the opcode that matches this name
      *  this will be called when the block is used
      */
-    myFirstBlock ({ MY_NUMBER, MY_STRING }) {
-        // example implementation to return a string
-        return MY_STRING + ' : doubled would be ' + (MY_NUMBER * 2);
+    Dec2Hex ({ MY_NUMBER }) {
+        // Return a string with hex representation of argument
+        return MY_NUMBER.toString(16);
+    }
+}
+
+    /**
+     * implementation of the block with the opcode that matches this name
+     *  this will be called when the block is used
+     */
+    Hex2Dec ({ MY_STRING }) {
+        // Return decimal value of hex argument
+        return parseInt(MY_STRING, 16);
+    }
+}
+
+    /**
+     * implementation of the block with the opcode that matches this name
+     *  this will be called when the block is used
+     */
+    Code2Char ({ MY_NUMBER }) {
+        // Return character represented by decimal code in argument
+        return String.fromCharCode(MY_NUMBER);
     }
 }
 
